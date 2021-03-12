3e1bcb1f52d2f791d760c9c125080d676a4bac1c
LadiFlow <adi_curt@hotmail.com>
2021-03-12 14:00:46 +0100

    Find the lenght of a string

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change the code bellow this line
+    var lastName
+    lastName = "Doe".length;
+    var lastnameLenght;
+    lastnameLenght = lastName;
+    // Only change the code above this line
+    return lastnameLenght;
+    }
+    
+    console.log(main());
+    module.exports = main;
