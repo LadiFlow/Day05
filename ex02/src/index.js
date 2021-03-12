5f3961826ff90ab1f6fcd62ae1cb49de522ececc
LadiFlow <adi_curt@hotmail.com>
2021-03-12 12:14:29 +0100

    Quoting string with single quotes'

---
+++ ex02/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change the code bellow this line
+    var myLink;
+    myLink = '"<a href=http://www.example.com" "target =_blank>Link</a"';
+    // Only change the code above this line
+    return myLink;
+    }
+    
+    console.log(main());
+    module.exports = main;
