477b836896a2032add399b800cdc249fccdfeedb
LadiFlow <adi_curt@hotmail.com>
2021-03-12 16:11:55 +0100

    Word blanks

---
+++ ex14/src/index.js
@@ -0,0 +1,14 @@
+function main() {
+    // Only change the code bellow this line
+    var myNoun = "dog";
+    var myVerb = "ran"
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    var wordBlanks;
+    wordBlanks = "The " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + ".";
+    // Only change the code above this line
+    return wordBlanks;
+    }
+    
+    console.log(main());
+    module.exports = main;
