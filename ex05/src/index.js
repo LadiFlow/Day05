c8ea7f78e3eb375781ca3fb5aa8117bb534fdbd5
LadiFlow <adi_curt@hotmail.com>
2021-03-12 13:12:06 +0100

    Concetenating strings with the plus equals operator

---
+++ ex05/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change the code bellow this line
+    var myLink = "Concetenation is the process of appending ";
+    myLink += "one string to the end of another string.";
+    // Only change the code above this line
+    return myLink;
+    }
+    
+    console.log(main());
+    module.exports = main;
