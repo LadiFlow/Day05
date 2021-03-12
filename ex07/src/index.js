b0dbd35e314424079656580ed239b9fe25e4d0ae
LadiFlow <adi_curt@hotmail.com>
2021-03-12 13:30:36 +0100

    Appending variables to strings

---
+++ ex07/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // Only change the code bellow this line
+    var someAdjective = "the best!";
+    var myString = "Mistral is ";
+    myString += someAdjective;
+    // Only change the code above this line
+    return myString;
+    }
+    
+    console.log(main());
+    module.exports = main;
