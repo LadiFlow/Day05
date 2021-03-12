d45c49e4793dc5ca56f164348a232b992a74dc46
LadiFlow <adi_curt@hotmail.com>
2021-03-12 13:24:00 +0100

    Constructing strings with variables

---
+++ ex06/src/index.js
@@ -0,0 +1,9 @@
+function main() {
+    // Only change the code bellow this line
+    var myName = "Pikachu";
+    var myString = "My name is " +myName+ " and I am well!"
+    // Only change the code above this line
+    return myString;
+    }
+    
+    console.log(main());
