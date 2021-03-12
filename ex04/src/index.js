d25f39f3eedfdfa43cb004c1d672a49850724210
LadiFlow <adi_curt@hotmail.com>
2021-03-12 13:03:58 +0100

    Concetenating string with plus operator

---
+++ ex04/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change the code bellow this line
+    var myLink;
+    myLink = "This is the first string." + " This is the second string.";
+    // Only change the code above this line
+    return myLink;
+    }
+    
+    console.log(main());
+    module.exports = main;
