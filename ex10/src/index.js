eec116877b034f049dfc50a8680644165881c1c0
LadiFlow <adi_curt@hotmail.com>
2021-03-12 14:41:47 +0100

    Understanding string immutability

---
+++ ex10/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change the code bellow this line
+    var myString = "Jello World";
+    myString = "Hello World";
+    // Only change the code above this line
+    return myString;
+    }
+    
+    console.log(main());
+    module.exports = main;
