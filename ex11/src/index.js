ee66014297970a654136695cd7b8f78d61bff44a
LadiFlow <adi_curt@hotmail.com>
2021-03-12 14:50:05 +0100

    Use bracket notation to find the nth characther in a string

---
+++ ex11/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change the code bellow this line
+    var lastName = "Tesla";
+    var thirdLetterOfLastName;
+    thirdLetterOfLastName = lastName[2];
+
+    // Only change the code above this line
+    return thirdLetterOfLastName;
+    }
+    
+    console.log(main());
+    module.exports = main;
