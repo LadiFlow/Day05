ec9fb3184d47fe9e1ba68740a3b679a1a386a21b
LadiFlow <adi_curt@hotmail.com>
2021-03-12 12:29:35 +0100

    Escape sequences in strings

---
+++ ex03/src/index.js
@@ -0,0 +1,10 @@
+function main() {
+    // Only change the code bellow this line
+    var myLink;
+    myLink = "FirstLine\n\tSecondLine\nThirdLine";
+    // Only change the code above this line
+    return myLink;
+    }
+    
+    console.log(main());
+    module.exports = main;
