c9c768ac657e2cfbc25731f84379e210d34bda89
LadiFlow <adi_curt@hotmail.com>
2021-03-12 14:29:38 +0100

    Using bracket notation to find the first caracther in a string

---
+++ ex09/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    // Only change the code bellow this line
+    var lastName = "Doe";
+    var firstLetterOfLastName = " ";
+    firstLetterOfLastName = lastName[0];
+    // Only change the code above this line
+    return firstLetterOfLastName;
+    }
+    
+    console.log(main());
+    module.exports = main;
